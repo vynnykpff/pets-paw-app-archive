df16ad01bdde5a7299f9117628257b3c9d834798
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-08-28 10:21:43 +0300

    fix(breeds): fix bugs with typization

--- src/components/Breeds/SpecificBreed/BreedDescription.tsx
+++ src/components/Breeds/SpecificBreed/BreedDescription.tsx
@@ -2,41 +2,42 @@ import React, {FC} from "react";
 import styles from "./BreedDescription.module.scss";
 
 interface Breed {
-  name: string;
-  weight: {
-    imperial: string;
-    metric: string;
-  };
-  temperament: string;
-  origin: string;
-  life_span: string;
+	name: string;
+	weight: {
+		imperial: string;
+		metric: string;
+	};
+	temperament: string;
+	origin: string;
+	life_span: string;
 }
 
 interface Props {
-  breed: Breed;
+	breed: Breed;
 }
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const BreedDescription: FC<Props> = ({breed: {name, weight, life_span, temperament, origin}}) => {
-  return <div className={styles.container}>
-    <h2 className={styles.breedName}>{name}</h2>
-    <div className={styles.breedInfoContainer}>
-      <div style={{maxWidth: "40%"}}>
-        <h4>Temperament : </h4><span>{temperament}</span>
-      </div>
-      <div>
-        <div>
-          <h4>Origin : <span>{origin}</span></h4>
-        </div>
-        <div>
-          <h4>Weight : <span>{weight?.metric}</span></h4>
-        </div>
-        <div>
-          <h4>Life span : <span>{life_span}</span></h4>
-        </div>
-      </div>
-    </div>
-  </div>
-}
+const BreedDescription: FC<Props> = ({breed: {name, weight, life_span: lifeSpan, temperament, origin}}) => {
+	return (
+		<div className={styles.container}>
+			<h2 className={styles.breedName}>{name}</h2>
+			<div className={styles.breedInfoContainer}>
+				<div style={{maxWidth: "40%"}}>
+					<h4>Temperament : </h4><span>{temperament}</span>
+				</div>
+				<div>
+					<div>
+						<h4>Origin : <span>{origin}</span></h4>
+					</div>
+					<div>
+						<h4>Weight : <span>{weight?.metric}</span></h4>
+					</div>
+					<div>
+						<h4>Life span : <span>{lifeSpan}</span></h4>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default BreedDescription;
